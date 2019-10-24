25ff2ff7b2c962a56ce2b14f2b6b48f35e44bac2
SetCome <45269359+SetCome@users.noreply.github.com>
2019-10-24 19:40:33 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,10 +14,16 @@ function toHTML(owca) {
 
 const tabela = document.querySelector("#owce");
 const imię = document.querySelector("#sheep-name");
+const szczęście = document.querySelector("#sheep-happy");
+const pływalność = document.querySelector("#sheep-sweam");
 
 function check( owca ){
 	const wpisaneImię = imię.value.trim()
-	if(wpisaneImię == "") return true;
+	const wpisaneSzczęście = szczęście.checked
+	const wpisanaPływalność = pływalność.checked
+	if(wpisaneImię == "" && !(owca.name.startsWith(wpisaneImię))) return false;
+	if(wpisaneSzczęście && (!owca.canSweam)) return false;
+	if(wpisanaPływalność && (!owca.canSweam)) return false;
 	return owca.name.startsWith( wpisaneImię );
 }
 
